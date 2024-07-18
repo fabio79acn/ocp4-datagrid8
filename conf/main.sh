ffdc83e0266ad6f9d60f9ad676ec52a6e00988a3
Fabio Martinelli <fabio.martinelli@accenture.com>
2024-07-18 17:01:13 +0200

    2nd draft

--- conf/main.sh
+++ conf/main.sh
@@ -8,4 +8,6 @@ oc delete  project coll-gestlck-be--datagrid-83-test-by-martinelli  && \
   oc create -f 20  && \
   ./25             && \
   echo OK          && \
-  timeout 20 oc get csv -w
+  timeout 20 oc get csv -w \
+  oc create -f 30
+
