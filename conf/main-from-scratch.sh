aeade00ab5235a12171a8c6a978923c25a46dae8
Fabio Martinelli <fabio.martinelli@accenture.com>
2024-08-09 18:39:32 +0200

    compreso che esiste la CLI di Datagrid

--- conf/main-from-scratch.sh
+++ conf/main-from-scratch.sh
@@ -19,17 +19,25 @@ sed   -s "s/myPROJ/${myPROJ}/g" 05 | oc create -f-  && \
   sed -s "s/myPROJ/${myPROJ}/g" 20 | oc create -f-  && \
   ./25             && \
   echo OK          && \
-  sleep 5          && \
   sed -s "s/myPROJ/${myPROJ}/g" 30 | oc create -f-  && \
+  oc wait --for condition=wellFormed --timeout=1000s infinispan/infinispan01
   sed -s "s/myPROJ/${myPROJ}/g" 40 | oc create -f-  && \
   sed -s "s/myPROJ/${myPROJ}/g" 50 | oc create -f-  && \
   readonly DONE="true" 
   
+readonly myPORTAL_USER="$(    oc get -n ${myPROJ} secret infinispan01-generated-operator-secret -o json | jq '.data.username' -r  | base64 -d)"
+readonly myPORTAL_PASSWORD="$(oc get -n ${myPROJ} secret infinispan01-generated-operator-secret -o json | jq '.data.password' -r  |base64 -d)"
+readonly myCLI_USER="$(       oc get -n ${myPROJ} extract secret/infinispan01-generated-secret  --to=-  |& egrep username |  cut  -d: -f2 | tr  -d ' ')"
+readonly myCLI_PASSWORD="$(   oc get -n ${myPROJ} extract secret/infinispan01-generated-secret  --to=-  |& egrep password |  cut  -d: -f2 | tr  -d ' ')"
+
+
 if [[ ${DONE} == "true" ]]; then
-  echo -n "DataGrid portal credentials; username:$(oc get -n ${myPROJ} secret infinispan01-generated-operator-secret -o json | jq '.data.username' -r  | base64 -d) password:$(oc get -n ${myPROJ} secret infinispan01-generated-operator-secret -o json | jq '.data.password' -r  |base64 -d)"
+  echo -n "DataGrid portal credentials; username:$myPORTAL_USER password:$myPORTAL_PASSWORD"
+  echo    "Datagrid CLI    credentials; username:$myCLI_USER password:$myCLI_PASSWORD"
   echo
-  echo "You still have to manually expose the admin svc as a https:// route in order to access the DataGrid portal !!"
+  echo "Be aware that you still have to manually expose the admin svc as a https:// route in order to access the DataGrid portal"
 fi
 
 [[ ${DONE} != "true" ]] && exit 1
+
 
--- conf/single-namespace-approve-1st-install-plan.sh
+++ conf/single-namespace-approve-1st-install-plan.sh
@@ -8,7 +8,7 @@ echo "Waiting for the 1st datagrid install plan to be present"
 # Function to display progress bar
 show_progress() {
   local PROG_BAR_WIDTH=50
-  local PROGRESS=$(($1 * $PROG_BAR_WIDTH / 1000))
+  local PROGRESS=$(($1 * $PROG_BAR_WIDTH / 100))
   local REMAINING=$(($PROG_BAR_WIDTH - $PROGRESS))
   local PROGRESS_BAR=$(printf "%${PROGRESS}s" | tr ' ' '#')
   local REMAINING_BAR=$(printf "%${REMAINING}s" | tr ' ' ' ')
@@ -22,18 +22,15 @@ for i in $(seq 0 100); do
   myINSTALLPLAN="$(oc get --sort-by=.metadata.creationTimestamp -n ${myPROJ} ip -o custom-columns=NAME:.metadata.name --no-headers=true | head -1)"
   [[ "${myINSTALLPLAN}" =~ "install" ]] && break
   show_progress $i
-  sleep 1
+  sleep 2
 done
-#echo -e "\nDone!"
+echo  "Install plan:${myINSTALLPLAN} is now available, processing it"
 
-
-oc -n ${myPROJ} patch                          installplan/${myINSTALLPLAN} --type merge -p '{"spec":{"approved":true}}'
-sleep 20
-echo "OK patch"
-oc -n ${myPROJ} wait --for=condition=Installed installplan/${myINSTALLPLAN}                      --timeout=300s
-sleep 20
-echo "OK 1/2 wait"
-oc -n ${myPROJ} wait --for=condition=Available deployment/infinispan-operator-controller-manager --timeout=600s
-sleep 20
-echo "OK 2/2 wait"
-
+set -x
+oc -n ${myPROJ} patch                            installplan/${myINSTALLPLAN} --type merge -p '{"spec":{"approved":true}}'
+oc -n ${myPROJ} wait --for=condition=Installed   installplan/${myINSTALLPLAN}                      --timeout=300s
+sleep 10
+oc -n ${myPROJ} wait --for=condition=Available   deployment/infinispan-operator-controller-manager --timeout=600s
+oc -n ${myPROJ} wait --for=condition=Initialized po -l app.kubernetes.io/name=infinispan-operator  --timeout=600s
+set +x
+echo
