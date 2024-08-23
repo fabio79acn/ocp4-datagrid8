3916c19d1ab07e8f77e5e2e9b1cf2e6e6ac26144
Fabio Martinelli <fabio.martinelli@accenture.com>
2024-08-23 10:57:58 +0200

    PVC can not be resized by interacting with infinispan CRD

--- day-2-ops/common.sh
+++ day-2-ops/common.sh
@@ -3,6 +3,6 @@
 # export myPROJ="<your-ocp-project>"
 # export myINFINISPAN=infinispan01
 ###############################################################################
-[ -z $myPROJ ]       && exit 1
-[ -z $myINFINISPAN ] && exit 1
+[ -z $myPROJ ]       && echo "You have to run: export myPROJ=\"<your-ocp-project>\"" && exit 1
+[ -z $myINFINISPAN ] && echo "You have to run: export myINFINISPAN=infinispan01"     && exit 1
 set -euo pipefail
--- day-2-ops/set-pvc-size.sh
+++ day-2-ops/set-pvc-size.sh
@@ -4,10 +4,10 @@
 [ ! -f common.sh ] && exit 1
 .      common.sh
 
-while getopts ":p:" opt; do
+while getopts ":s:" opt; do
   case ${opt} in
-    p)
-      echo "Option -p was triggered with argument: $OPTARG"
+    s)
+      echo "Option -s was triggered with argument: $OPTARG"
       readonly myPVC_SIZE=$OPTARG
       ;;
     \?)
@@ -22,5 +22,13 @@ while getopts ":p:" opt; do
 done
 
 
+#RH Case 03881959 :-(
+#oc -n $myPROJ patch infinispan/${myINFINISPAN} --type=merge -p "{\"spec\": {\"service\": {\"container\": {\"storage\": \"${myPVC_SIZE}Gi\"}}}}"
 
-oc -n $myPROJ patch infinispan/${myINFINISPAN} --type=merge -p "{\"spec\": {\"service\": {\"container\": {\"storage\": \"${myPVC_SIZE}Gi\"}}}}"
+# pvc.spec.resources.requests.storage
+readonly myPVCs="$(oc -n $myPROJ get pvc -l clusterName=infinispan01 -o name | tr  "\n" " ")"
+[ -z "$myPVCs" ] && exit 1
+for PVC in $myPVCs; do 
+  oc -n $myPROJ patch ${PVC} --type=merge -p "{\"spec\": {\"resources\": {\"requests\": {\"storage\": \"${myPVC_SIZE}Gi\"}}}}"
+done
+
