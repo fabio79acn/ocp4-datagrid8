259a3e8f330b1378ff25511e3299fab1425634cc
Fabio Martinelli <fabio.martinelli@accenture.com>
2024-08-23 11:00:07 +0200

    1st DRAFT of declaritive tests

--- tests/declarative/test-02.sh
+++ tests/declarative/test-02.sh
@@ -12,4 +12,4 @@ egrep $myTEST_NAME $myFILE >/dev/null  2>&1
 [ $? -ne 0     ] && exit 1
 
 
-sed -e "s/myPROJ/${myPROJ}/" $myFILE  | oc create -f- 
+for x in $(seq 10 20) ; do sed -e "s/myPROJ/${myPROJ}/" -e "s/$myTEST_NAME/declarative-test-$x/g" $myFILE  | oc create -f-  ; done
